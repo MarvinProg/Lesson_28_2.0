eaa470957577d4e1c234fdb27079ad4b22655702
lomakin <lomakin-as@mail.ru>
2022-03-16 17:12:28 +0300

    add: init db method

--- app.rb
+++ app.rb
@@ -2,18 +2,33 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db_create = SQLite3::Database.new 'my_db.db'
+  @db_create.results_as_hash = true
+  return @db_create
+end
+
+before do 
+  init_db
+end
+
+configure do 
+
+end
 
 get '/' do
-	# erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
-	erb :index
+  # erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb :index
 end
 
 get '/new' do 
-	erb :new
+  erb :new
 end
 
 post '/new' do 
-	@content_text = params[:content_text]
+  @content_text = params[:content_text]
 
-	erb :new
+  erb :new
 end
