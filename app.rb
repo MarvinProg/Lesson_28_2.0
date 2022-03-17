53c3fac56f4c815f3e0d7c1f46d93650ba4b9ae3
lomakin <lomakin-as@mail.ru>
2022-03-17 17:22:45 +0300

    add: show posts in page

--- app.rb
+++ app.rb
@@ -25,6 +25,9 @@ end
 
 get '/' do
   # erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+
+  @db = init_db
+  @db_result = @db.execute('select * from Posts order by id desc')
   erb :index
 end
 
